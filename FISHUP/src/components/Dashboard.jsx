bf0b2891749fe4c79b0f51a984003eebd140ece4
Markoco8 <a6520150028@utch.edu.mx>
2024-02-09 18:19:09 -0600

    Se mejoro la presentacion de los datos de temp y ph

--- FISHUP/src/components/Dashboard.jsx
+++ FISHUP/src/components/Dashboard.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 function Dashboard() {
   return (
-    <div>
-      <section className="flex flex-row">
-        <table
-          id="tablaD"
-          className="border-2 border-lime-900 rounded-lg border-separate border-spacing-4 md:table-fixed bg-emerald-950/[.5]"
-        >
-          <thead>
-            <tr>
-              <th className="">Temperatura c°</th>
-              <th className="">Nivel de PH</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="" id="temp">
-                Temp
-              </td>
-              <td className="" id="ph">
-                PH
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
+    <div class="w-full max-w-96 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div class="items-center justify-between ">
+        <h5 class="text-xl font-bold leading-none text-teal-200 uppercase">
+          Peceras
+        </h5>
+      </div>
+      <div id="peceras">
+        <ul class="uppercase divide-y divide-gray-200 dark:divide-gray-700">
+          <li class="">
+            <div class="flex items-center">
+              <h1 className="mr-10" id="nombrePecera">Nombre</h1>
+              <div class="flex-1 text-gray-900 dark:text-white">
+                <p className="text-xl">Temperatura</p>
+                <p id="temp" className="text-lg">
+                  8
+                </p>
+              </div>
+              <div class="text-gray-900 dark:text-white">
+                <p className="text-xl ">pH</p>
+                <p id="ph" className="text-lg">
+                  7
+                </p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>Division</div>
     </div>
   );
 }
