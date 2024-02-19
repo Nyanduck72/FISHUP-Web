bdc31198f9bff514950c034975412a20a472a704
Markoco8 <a6520150028@utch.edu.mx>
2024-02-19 17:09:17 -0600

    Update Card,jsx
    Mejora de Tarjeta

--- FISHUP/src/assets/components/Card.jsx
+++ FISHUP/src/assets/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 function Card() {
   return (
-    <div class="w-full max-w-96 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <div class="items-center justify-between ">
         <h5 class="text-xl font-bold leading-none text-teal-200 uppercase">
           Peceras
@@ -11,16 +11,18 @@ function Card() {
         <ul class="uppercase divide-y divide-gray-200 dark:divide-gray-700">
           <li class="">
             <div class="flex items-center">
-              <h1 className="mr-10" id="nombrePecera">Nombre</h1>
+              <h1 className="mr-10" id="nombrePecera">
+                Nombre
+              </h1>
               <div class="flex-1 text-gray-900 dark:text-white">
-                <p className="text-xl">Temperatura</p>
-                <p id="temp" className="text-lg">
+                <p className="text-xl mr-10">Temperatura</p>
+                <p id="temp" className="text-lg dark:text-blue-500">
                   8
                 </p>
               </div>
               <div class="text-gray-900 dark:text-white">
-                <p className="text-xl ">pH</p>
-                <p id="ph" className="text-lg">
+                <p className="text-xl mr-10">pH</p>
+                <p id="ph" className="text-lg dark:text-blue-500">
                   7
                 </p>
               </div>
@@ -28,7 +30,9 @@ function Card() {
           </li>
         </ul>
       </div>
-      <div>Division</div>
+      <div>
+        -----------------------------------------------------------------------------
+      </div>
     </div>
   );
 }
